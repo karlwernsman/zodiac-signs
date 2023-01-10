d2ed261a807d751921cbfeb93f6dd1d1b9eef02c
karl wernsman <wernsmank@gmail.com>
2023-01-10 15:17:16 -0800

    mapping thru zodiac data

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './Main.css';
+import ZodiacCard from '../Zodiac/Zodiac.js';
+import { zodiacs } from '../../data.js';
 
 export default function Main() {
-  return <main>Main</main>;
+  return (
+    <main>
+      {zodiacs.map((zodiac) => (
+        <ZodiacCard key={zodiac.id} image={zodiac.name} name={zodiac.name} dates={zodiac.dates} />
+      ))}
+    </main>
+  );
 }
---
+++ src/components/Zodiac/Zodiac.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Zodiac.css';
+
+export default function Zodiac(props) {
+  return <div className="zodiac"></div>;
+}
